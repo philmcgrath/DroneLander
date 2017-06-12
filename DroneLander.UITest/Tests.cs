e7ca2901332b0b6f80bf8ef586b60f97f73c82d6
Phil McGrath <phil.mcgrath@trustpower.co.nz>
2017-06-12 20:35:14 +1200

    Successful build after Azure DB backend service implemented

---
+++ DroneLander.UITest/Tests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace DroneLander.UITest
+{
+    [TestFixture(Platform.Android)]
+    [TestFixture(Platform.iOS)]
+    public class Tests
+    {
+        IApp app;
+        Platform platform;
+
+        public Tests(Platform platform)
+        {
+            this.platform = platform;
+        }
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            app = AppInitializer.StartApp(platform);
+        }
+
+        [Test]
+        public void AppLaunches()
+        {
+            app.Tap(x => x.Button("Start"));
+            app.Screenshot("The app in progress.");
+            //app.Flash(x => x.Text("Sign In"));
+            app.SetOrientationLandscape();
+            app.PressVolumeDown();
+            app.PressVolumeDown();
+            app.SetOrientationPortrait();
+            app.Flash(x => x.Button("Reset"));
+            app.PressVolumeUp();
+            app.PressVolumeUp();
+        }
+    }
+}
+
